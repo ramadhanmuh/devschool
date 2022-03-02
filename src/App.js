1eed9ceeb4a5a2ebff13ef289f0f7d0268339b99
Muhammad Ramadhan <muhrama082@gmail.com>
2022-03-02 14:42:35 +0700

    Persiapakan layout

--- src/App.js
+++ src/App.js
@@ -3,23 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      members: [{}, {}, {}, {}]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <h1>Codepolitan DevSchool</h1>
+        <div className='row'>
+          <div className='col-md-6 border border-dark'>
+            <h2>Member</h2>
+            <div className='row'>
+              {this.state.members.map((member, index) => 
+                <div className='col-md-6'>
+                  <div className='card' style={{margin: 10}}>
+                    <div className='card-body'>
+                      <h5 className='card-title'>ID Member</h5>
+                      <h5 className='card-title'>First Name</h5>
+                      <h5 className='card-title'>Last Name</h5>
+                      <button className='btn btn-primary'>
+                        Edit
+                      </button>
+                      <button className='btn btn-danger'>
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className='col-md-6 border border-dark'>
+            <h2>Form</h2>
+            <form>
+              <div className='form-group'>
+                <label>First Name</label>
+                <input type="text" className='form-control' />
+              </div>
+              <div className='form-group'>
+                <label>Last Name</label>
+                <input type="text" className='form-control' />
+              </div>
+              <button type="submit" className='btn btn-primary'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
